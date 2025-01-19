fb86f7ad07d10c6e6be0e3445b97a615ec4ad3d1
tobezero01 <ducnhuad@gmail.com>
2025-01-20 04:11:03 +0700

    change 2

--- Web_Practice_1/Web_Practice_1/Program.cs
+++ Web_Practice_1/Web_Practice_1/Program.cs
@@ -1,6 +1,35 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+//app.Run(async (HttpContext context) =>
+//{
+//	string path = context.Request.Path;
+//	string method = context.Request.Method;
+
+//	context.Response.Headers["Content-type"] = "text/html";
+//	await context.Response.WriteAsync($"<p>{path}</p>");
+//	await context.Response.WriteAsync($"<p>{method}</p>");
+//});
+
+// middleware 1
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+	await context.Response.WriteAsync("Hello");
+	await next(context);
+});
+
+// middleware 2
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+	await context.Response.WriteAsync("Hello");
+	await next(context);
+});
+// middleware 3
+app.Run(async (HttpContext context) =>
+{
+	await context.Response.WriteAsync(" Hello Again");
+});
 
 app.Run();
--- Web_Practice_1/Web_Practice_1/obj/Debug/net8.0/Web_Practice_1.AssemblyInfo.cs
+++ Web_Practice_1/Web_Practice_1/obj/Debug/net8.0/Web_Practice_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_Practice_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41b55fa269a16e92769d9adee122ab9ca2d78e10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7db8ea964bc5bc774ba56712c66166c1a1f34dea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_Practice_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_Practice_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
