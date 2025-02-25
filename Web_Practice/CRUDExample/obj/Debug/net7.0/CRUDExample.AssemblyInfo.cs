a0a793a9313b4358ee5e23674a2552925cf6cf47
tobezero01 <ducnhuad@gmail.com>
2025-02-26 00:00:31 +0700

    add new project for clean architecture

--- Web_Practice/CRUDExample/obj/Debug/net7.0/CRUDExample.AssemblyInfo.cs
+++ Web_Practice/CRUDExample/obj/Debug/net7.0/CRUDExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab28cc407a106862ca3b89634ad9d413c7e6533f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ad1f8a2ce42b09f2f63d357e3627e3de90e4a26")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
